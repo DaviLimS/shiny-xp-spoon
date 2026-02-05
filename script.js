8bdf83b15b1d60223e63f078a618bd48702db5f1
Davi <davilimasoares1@outlook.com>
2026-02-05 10:56:04 -0300

    I'm working on the levelUpper function

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+function startGame() {
+    let chalenge = prompt('Qual Level será o objetivo? ');
+}
+
 function createPlayer(nickname, skill) {
     let level = 0;
+    let roof = 0;
     const getLevel = () => level;
     const increaseLevel = () => {
         level++;
     }
+    const levelUpper = () => {
+        
+    }  
 
     return {nickname, skill, getLevel, increaseLevel}
-}
-
-function startGame() {
-    let chalenge = prompt('Qual Level será o objetivo? ');
 }
